69d249f52ed0e06115e8033d4163a5d10df684cb
HIKMATFATHURRAHMAN <hikmatfathurrahman5@gmail.com>
2021-12-21 01:10:20 +0700

    Add project files.

---
+++ ClientReservasi_20190140088/ServiceReference1/ServiceClient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClientReservasi_20190140088.ServiceReference1
+{
+    internal class ServiceClient
+    {
+        internal object pemesanan(string iDPemesanan, string namaCustomer, string noTelpon, int jumlahPemesanan, string idLokasi)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object editPemesanan(string iDPemesanan, string namaCustomer, string noTelpon)
+        {
+            throw new NotImplementedException();
+        }
+
+       
+        internal object deletePemesanan(string iDPemesanan)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object Pemesanan1()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ServiceReference1/ServiceClient.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ServiceReference1
+{
+    internal class ServiceClient
+    {
+        public ServiceClient()
+        {
+        }
+
+        internal object pemesanan(string iDPemesanan, string namaCustomer, string noTelpon, int jumlahPemesanan, object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object editPemesanan(string iDPemesanan, string namaCustomer, string noTelpon)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object deletePemesanan(string iDPemesanan)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object Pemesanan()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object Pemesanan1()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
